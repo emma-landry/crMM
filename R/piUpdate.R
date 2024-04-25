91cb63689e2e6884300e31e047b82a0c6fb310b9
Emma Landry <emma.landry17@imperial.ac.uk>
2024-04-25 11:48:37 -0700

    update of pi

---
+++ R/piUpdate.R
@@ -0,0 +1,283 @@
+piUpdate_NoWarp <- function(t, y, c, gamma1, gamma2, pi, knots_shape, degree = 3, var_e,
+                            alpha, rescale = T, label1 = NULL, label2 = NULL, tuning_param = 1000) {
+  n <- length(t)
+  N <- length(c)
+
+  if (nrow(y) != N){
+    stop("The number of rows in 'y' must match the length of 'c'.")
+  }
+
+  if (ncol(y) != n) {
+    stop("The number columns in 'y' must match the length of 't'.")
+  }
+
+  if (!is.null(label1)) {
+    if (!(all(label1 >= 1 & label1 <= N))) {
+      stop("The indexes in 'label1' fall out of the range of the total number of observations.")
+    }
+
+    if (!is.null(label2)) {
+      if (length(intersect(label1, label2)) > 0) {
+        stop("Observations cannot be labelled as belonging fully to both feature 1 and feature 2.")
+      }
+    }
+  }
+
+  if (!is.null(label2)) {
+    if (!(all(label2 >= 1 & label2 <= N))) {
+      stop("The indexes in 'label2' fall out of the range of the total number of observations.")
+    }
+  }
+
+  for (i in 1:N) {
+    if (!is.null(label1) & (i %in% label1)) {
+      next
+    }
+
+    if (!is.null(label2) & (i %in% label2)) {
+      next
+    }
+
+    pi_old <- pi[i, ]
+
+    if(pi_old[1] < 0.0001) {
+      pi_old[1] <- 0.01
+      pi_old[2] <- 1 - pi_old[1]
+    }
+
+    if (pi_old[2] < 0.0001) {
+      pi_old[2] <- 0.01
+      pi_old[1] <- 1 - pi_old[2]
+    }
+
+    pi_new <- LaplacesDemon::rdirichlet(n = 1, alpha = tuning_param * pi_old)
+
+    if(pi_new[1] < 0.0001) {
+      pi_new[1] <- 0.01
+      pi_new[2] <- 1 - pi_new[1]
+    }
+
+    if (pi_new[2] < 0.0001) {
+      pi_new[2] <- 0.01
+      pi_new[1] <- 1 - pi_new[2]
+    }
+
+    modelMean_old <- meanNoWarp(t, c[i], gamma1, gamma2, pi_old, knots_shape, degree, intercept)
+    modelMean_new <- meanNoWarp(t, c[i], gamma1, gamma2, pi_new, knots_shape, degree, intercept)
+    y_i <- y[i, ]
+
+    P1 <- -1 / (2 * var_e) * sum((y_i - modelMean_new) ^ 2) +
+          (alpha[1] - 1) * log(pi_new[1]) +
+          (alpha[2] - 1) * log(pi_new[2])
+    Q1 <- LaplacesDemon::ddirichlet(pi_new, alpha = tuning_param * pi_old, log = T)
+
+    P0 <- -1 / (2 * var_e) * sum((y_i - modelMean_old) ^ 2) +
+          (alpha[1] - 1) * log(pi_old[1]) +
+          (alpha[2] - 1) * log(pi_old[2])
+    Q0 <- LaplacesDemon::didirichlet(pi_old, alpha = tuning_param * pi_new, log = T)
+
+    ratio <- (P1 - Q1) - (P0 - Q0)
+
+    if (log(runif(1)) < ratio) {
+      pi_old <- pi_new
+    }
+    pi[i, ] <- pi_old
+  }
+
+  if (rescale == T) {
+    pi[, 1] <- (pi[, 1] - min(pi[, 1])) / (max(pi[, 1]) - min(pi[, 1]))
+    pi[, 2] <- 1 - pi[, 1]
+  }
+  return(pi)
+}
+
+piUpdate_Warp <- function(t, y, c, phi, rho, tt_basis, gamma1, gamma2, pi, knots_shape, degree = 3, var_e,
+                            alpha, rescale = T, label1 = NULL, label2 = NULL, tuning_param = 1000) {
+  n <- length(t)
+  N <- length(c)
+
+  if (nrow(y) != N){
+    stop("The number of rows in 'y' must match the length of 'c'.")
+  }
+
+  if (ncol(y) != n) {
+    stop("The number columns in 'y' must match the length of 't'.")
+  }
+
+  if (!is.null(label1)) {
+    if (!(all(label1 >= 1 & label1 <= N))) {
+      stop("The indexes in 'label1' fall out of the range of the total number of observations.")
+    }
+
+    if (!is.null(label2)) {
+      if (length(intersect(label1, label2)) > 0) {
+        stop("Observations cannot be labelled as belonging fully to both feature 1 and feature 2.")
+      }
+    }
+  }
+
+  if (!is.null(label2)) {
+    if (!(all(label2 >= 1 & label2 <= N))) {
+      stop("The indexes in 'label2' fall out of the range of the total number of observations.")
+    }
+  }
+
+  for (i in 1:N) {
+    if (!is.null(label1) & (i %in% label1)) {
+      next
+    }
+
+    if (!is.null(label2) & (i %in% label2)) {
+      next
+    }
+
+    pi_old <- pi[i, ]
+
+    if(pi_old[1] < 0.0001) {
+      pi_old[1] <- 0.01
+      pi_old[2] <- 1 - pi_old[1]
+    }
+
+    if (pi_old[2] < 0.0001) {
+      pi_old[2] <- 0.01
+      pi_old[1] <- 1 - pi_old[2]
+    }
+
+    pi_new <- LaplacesDemon::rdirichlet(n = 1, alpha = tuning_param * pi_old)
+
+    if(pi_new[1] < 0.0001) {
+      pi_new[1] <- 0.01
+      pi_new[2] <- 1 - pi_new[1]
+    }
+
+    if (pi_new[2] < 0.0001) {
+      pi_new[2] <- 0.01
+      pi_new[1] <- 1 - pi_new[2]
+    }
+
+    modelMean_old <- meanWarp(t, c[i], phi[i, ], rho, tt_basis, gamma1, gamma2, pi_old,
+                              knots_shape, degree, intercept)
+    modelMean_new <- meanWarp(t, c[i], phi[i, ], rho, tt_basis, gamma1, gamma2, pi_new,
+                              knots_shape, degree, intercept)
+    y_i <- y[i, ]
+
+    P1 <- -1 / (2 * var_e) * sum((y_i - modelMean_new) ^ 2) +
+      (alpha[1] - 1) * log(pi_new[1]) +
+      (alpha[2] - 1) * log(pi_new[2])
+    Q1 <- LaplacesDemon::ddirichlet(pi_new, alpha = tuning_param * pi_old, log = T)
+
+    P0 <- -1 / (2 * var_e) * sum((y_i - modelMean_old) ^ 2) +
+      (alpha[1] - 1) * log(pi_old[1]) +
+      (alpha[2] - 1) * log(pi_old[2])
+    Q0 <- LaplacesDemon::didirichlet(pi_old, alpha = tuning_param * pi_new, log = T)
+
+    ratio <- (P1 - Q1) - (P0 - Q0)
+
+    if (log(runif(1)) < ratio) {
+      pi_old <- pi_new
+    }
+    pi[i, ] <- pi_old
+  }
+
+  if (rescale == T) {
+    pi[, 1] <- (pi[, 1] - min(pi[, 1])) / (max(pi[, 1]) - min(pi[, 1]))
+    pi[, 2] <- 1 - pi[, 1]
+  }
+  return(pi)
+}
+
+piUpdate_Warp_alt <- function(t, y, c, phi, tt_basis, gamma1, gamma2, pi, knots_shape, degree = 3, var_e,
+                          alpha, rescale = T, label1 = NULL, label2 = NULL, tuning_param = 1000) {
+  n <- length(t)
+  N <- length(c)
+
+  if (nrow(y) != N){
+    stop("The number of rows in 'y' must match the length of 'c'.")
+  }
+
+  if (ncol(y) != n) {
+    stop("The number columns in 'y' must match the length of 't'.")
+  }
+
+  if (!is.null(label1)) {
+    if (!(all(label1 >= 1 & label1 <= N))) {
+      stop("The indexes in 'label1' fall out of the range of the total number of observations.")
+    }
+
+    if (!is.null(label2)) {
+      if (length(intersect(label1, label2)) > 0) {
+        stop("Observations cannot be labelled as belonging fully to both feature 1 and feature 2.")
+      }
+    }
+  }
+
+  if (!is.null(label2)) {
+    if (!(all(label2 >= 1 & label2 <= N))) {
+      stop("The indexes in 'label2' fall out of the range of the total number of observations.")
+    }
+  }
+
+  for (i in 1:N) {
+    if (!is.null(label1) & (i %in% label1)) {
+      next
+    }
+
+    if (!is.null(label2) & (i %in% label2)) {
+      next
+    }
+
+    pi_old <- pi[i, ]
+
+    if(pi_old[1] < 0.0001) {
+      pi_old[1] <- 0.01
+      pi_old[2] <- 1 - pi_old[1]
+    }
+
+    if (pi_old[2] < 0.0001) {
+      pi_old[2] <- 0.01
+      pi_old[1] <- 1 - pi_old[2]
+    }
+
+    pi_new <- LaplacesDemon::rdirichlet(n = 1, alpha = tuning_param * pi_old)
+
+    if(pi_new[1] < 0.0001) {
+      pi_new[1] <- 0.01
+      pi_new[2] <- 1 - pi_new[1]
+    }
+
+    if (pi_new[2] < 0.0001) {
+      pi_new[2] <- 0.01
+      pi_new[1] <- 1 - pi_new[2]
+    }
+
+    modelMean_old <- meanWarp_alt(t, c[i], phi[i, ], tt_basis, gamma1, gamma2, pi_old,
+                              knots_shape, degree, intercept)
+    modelMean_new <- meanWarp_alt(t, c[i], phi[i, ], tt_basis, gamma1, gamma2, pi_new,
+                              knots_shape, degree, intercept)
+    y_i <- y[i, ]
+
+    P1 <- -1 / (2 * var_e) * sum((y_i - modelMean_new) ^ 2) +
+      (alpha[1] - 1) * log(pi_new[1]) +
+      (alpha[2] - 1) * log(pi_new[2])
+    Q1 <- LaplacesDemon::ddirichlet(pi_new, alpha = tuning_param * pi_old, log = T)
+
+    P0 <- -1 / (2 * var_e) * sum((y_i - modelMean_old) ^ 2) +
+      (alpha[1] - 1) * log(pi_old[1]) +
+      (alpha[2] - 1) * log(pi_old[2])
+    Q0 <- LaplacesDemon::didirichlet(pi_old, alpha = tuning_param * pi_new, log = T)
+
+    ratio <- (P1 - Q1) - (P0 - Q0)
+
+    if (log(runif(1)) < ratio) {
+      pi_old <- pi_new
+    }
+    pi[i, ] <- pi_old
+  }
+
+  if (rescale == T) {
+    pi[, 1] <- (pi[, 1] - min(pi[, 1])) / (max(pi[, 1]) - min(pi[, 1]))
+    pi[, 2] <- 1 - pi[, 1]
+  }
+  return(pi)
+}
+
--- R/variancesUpdate.R
+++ R/variancesUpdate.R
@@ -34,3 +34,41 @@ var_eUpdate_NoWarp <- function(t, y, c, gamma1, gamma2, pi, knots_shape, degree 
   var_e <- 1 / stats::rgamma(n = 1, shape = a_e + n * N / 2, rate = b_e + squaredSums / 2)
   return(var_e)
 }
+
+var_eUpdate_Warp <- function(t, y, c, phi, rho, tt_basis, gamma1, gamma2, pi, knots_shape,
+                             degree = 3, intercept = F, a_e, b_e) {
+  n <- length(t)
+  N <- length(c)
+
+  if (nrow(y) != N){
+    stop("The number of rows in 'y' must match the length of 'c'.")
+  }
+
+  if (ncol(y) != n) {
+    stop("The number columns in 'y' must match the length of 't'.")
+  }
+
+  modelMean <- meanWarp(t, c, phi, rho, tt_basis, gamma1, gamma2, pi, knots_shape, degree, intercept)
+  squaredSums <- sum(rowSums((y - modelMean) ^ 2))
+  var_e <- 1 / stats::rgamma(n = 1, shape = a_e + n * N / 2, rate = b_e + squaredSums / 2)
+  return(var_e)
+}
+
+var_eUpdate_Warp_alt <- function(t, y, c, phi, tt_basis, gamma1, gamma2, pi, knots_shape,
+                             degree = 3, intercept = F, a_e, b_e) {
+  n <- length(t)
+  N <- length(c)
+
+  if (nrow(y) != N){
+    stop("The number of rows in 'y' must match the length of 'c'.")
+  }
+
+  if (ncol(y) != n) {
+    stop("The number columns in 'y' must match the length of 't'.")
+  }
+
+  modelMean <- meanWarp_alt(t, c, phi, tt_basis, gamma1, gamma2, pi, knots_shape, degree, intercept)
+  squaredSums <- sum(rowSums((y - modelMean) ^ 2))
+  var_e <- 1 / stats::rgamma(n = 1, shape = a_e + n * N / 2, rate = b_e + squaredSums / 2)
+  return(var_e)
+}
